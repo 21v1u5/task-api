6dcd566c30be01e22afce1810bbc17d761474825
21v1u5 <4raujopenh4@gmail.com>
2026-07-02 11:57:08 -0300

    feat: rotas configuradas e autenticacao configuradas

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    
+    public function index()
+    {
+        
+        $projects = Project::paginate(10);
+        
+        return response()->json($projects, 200);
+    }
+
+    
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'client_name' => 'required|string|max:255',
+        ]);
+
+        $project = Project::create($validated);
+
+        return response()->json([
+            'message' => 'Projeto criado com sucesso.',
+            'project' => $project
+        ], 201); // 201 Created
+    }
+
+    
+    public function show(Project $project)
+    {
+        return response()->json($project, 200);
+    }
+
+    
+    public function update(Request $request, Project $project)
+    {
+        $validated = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'client_name' => 'sometimes|string|max:255',
+        ]);
+
+        $project->update($validated);
+
+        return response()->json([
+            'message' => 'Projeto atualizado com sucesso.',
+            'project' => $project
+        ], 200);
+    }
+
+    
+    public function destroy(Project $project)
+    {
+        $project->delete();
+
+        return response()->json([
+            'message' => 'Projeto excluído com sucesso.'
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController; // Não esqueça deste import
 
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -12,7 +13,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     
     
-    Route::get('/me', function (Request $request) {
-        return $request->user();
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::get('/projects/{project}', [ProjectController::class, 'show']);
+    
+    
+    Route::middleware('admin')->group(function () {
+        Route::post('/projects', [ProjectController::class, 'store']);
+        Route::put('/projects/{project}', [ProjectController::class, 'update']);
+        Route::delete('/projects/{project}', [ProjectController::class, 'destroy']);
     });
 });
